6926894426691f881256d7a8c282f82ac1c8078b
wuzhiqiang <89005707@sfmail.sf-express.com>
2019-07-31 15:15:23 +0800

    kafka配置文件添加

---
+++ spring-boot-kafka/src/main/java/com/wzq/kafka/SpringBootKafkaApplication.java
@@ -0,0 +1,17 @@
+package com.wzq.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author
+ */
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+    }
+
+}
