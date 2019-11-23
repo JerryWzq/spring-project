db3f17f13a3a6493b1d8a95223e571aff9dd2395
jerry <1344731479@qq.com>
2019-11-24 00:11:25 +0800

    maven wagon插件使用

---
+++ spring-boot-scp/src/main/java/com/wzq/spring/SpringBootScpApplication.java
@@ -0,0 +1,13 @@
+package com.wzq.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootScpApplication.class, args);
+    }
+
+}
---
+++ spring-boot-scp/src/test/java/com/wzq/spring/SpringBootScpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzq.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootScpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
